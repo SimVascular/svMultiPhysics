dc74b12bd21267544e6e49c73436ea6d85fdb298
Michele Bucelli <mbucelli@utexas.edu>
2026-04-24 16:55:24 -0500

    First implementation of abstract IonicModel class

---
+++ Code/Source/solver/ionic_model.cpp
@@ -0,0 +1,176 @@
+// SPDX-FileCopyrightText: Copyright (c) Stanford University, The Regents of the
+// University of California, and others. SPDX-License-Identifier: BSD-3-Clause
+
+#include "ionic_model.h"
+
+#include "mat_fun.h"
+
+void IonicModel::init(const int nX, Vector<double> &X, double X0) const {
+  X = X0;
+}
+
+void IonicModel::init(const int nX, Vector<double> &X,
+                      const Vector<double> &X0) const {
+  X = X0;
+}
+
+void IonicModel::integ_cn2(const int nX, Vector<double> &Xn, const double Ts,
+                           const double Ti, const double Istim,
+                           const double Ksac, Vector<int> &IPAR,
+                           Vector<double> &RPAR) const {
+  // @todo The nX argument can probably be removed and replaced by the length of
+  // the state vector Xn.
+
+  const int itMax = IPAR(0);   // Maximum iterations for nonlinear solver.
+  const double atol = RPAR(0); // Absolute tolerance for nonlinear solver.
+  const double rtol = RPAR(1); // Relative tolerance for nonlinear solver.
+
+  // Stretch-activated current.
+  const double Isac = Ksac * (Vrest - Xn(0));
+
+  // Rescale current time, timestep and transmembrane potential by the
+  // model-specific scaling factors.
+  const double dt = Ti / Tscale;
+  const double t = Ts / Tscale + dt;
+  Xn(0) = (Xn(0) - Voffset) / Vscale;
+
+  // Total external stimulus current is the sum of the applied current and the
+  // SAC current.
+  const double fext = (Istim + Isac) * Tscale / Vscale;
+
+  const auto Im = mat_fun::mat_id(nX);
+
+  // Evaluate the right-hand side function for the system at the old time.
+  Vector<double> fn(nX);
+  getf(nX, Xn, fn, fext);
+
+  int k = 0;    // Current nonlinear iteration index.
+  auto Xk = Xn; // Current solution. This copy is probably unnecessary.
+
+  // @todo The following flags should be given meaningful names.
+  // Flag indicating whether the maximum number of iterations was reached.
+  bool l1 = false;
+
+  // Flag indicating whether absolute tolerance is satisfied.
+  bool l2 = false;
+
+  // Flag indicating whether relative tolerance is satisfied.
+  bool l3 = false;
+
+  constexpr double eps = std::numeric_limits<double>::epsilon();
+
+  while (true) {
+    ++k;
+
+    // Evaluate the right-hand side function for the system at the new time and
+    // current nonlinear iteration.
+    Vector<double> fk(nX);
+    getf(nX, Xk, fk, fext);
+
+    auto rK = Xk - Xn - 0.5 * dt * (fk + fn);
+
+    double rmsA = 0.0;
+    double rmsR = 0.0;
+
+    for (int i = 0; i < nX; ++i) {
+      rmsA += rK(i) * rK(i);
+
+      const double r_i = rK(i) / (Xk(i) + eps);
+      rmsR += r_i * r_i;
+    }
+
+    rmsA = sqrt(rmsA / nX);
+    rmsR = sqrt(rmsR / nX);
+
+    l1 = (k >= itMax);
+    l2 = (rmsA <= atol);
+    l3 = (rmsR <= rtol);
+
+    if (l1 || l2 || l3)
+      break;
+
+    Array<double> JAC(nX, nX);
+    getj(nX, Xk, JAC, Ksac * Tscale);
+
+    JAC = Im - 0.5 * dt * JAC;
+    JAC = mat_fun::mat_inv(JAC, nX);
+    rK = mat_fun::mat_mul(JAC, rK);
+    Xk = Xk - rK;
+  }
+
+  Xn = Xk;
+
+  // @todo This call seems unnecessary, since fn is not used after this point.
+  getf(nX, Xn, fn, fext);
+
+  // Bring the potential variable back to dimensional units.
+  Xn(0) = Xn(0) * Vscale + Voffset;
+
+  if (!l2 && !l3) {
+    IPAR(1) = IPAR(1) + 1;
+  }
+}
+
+void IonicModel::integ_fe(const int nX, Vector<double> &X, const double Ts,
+                          const double Ti, const double Istim,
+                          const double Ksac) const {
+
+  // Rescale current time, timestep and transmembrane potential by the
+  // model-specific scaling factors.
+  const double t = Ts / Tscale;
+  const double dt = Ti / Tscale;
+
+  // Stretch-activated current.
+  const double Isac = Ksac * (Vrest - X(0));
+
+  X(0) = (X(0) - Voffset) / Vscale;
+
+  const double fext = (Istim + Isac) * Tscale / Vscale;
+
+  Vector<double> f(nX);
+  getf(nX, X, f, fext);
+
+  X = X + dt * f;
+
+  // Bring the potential variable back to dimensional units.
+  X(0) = X(0) * Vscale + Voffset;
+}
+
+void IonicModel::integ_rk(const int nX, Vector<double> &X, const double Ts,
+                          const double Ti, const double Istim,
+                          const double Ksac) const {
+  // Stretch-activated current.
+  const double Isac = Ksac * (Vrest - X(0));
+
+  // Rescale current time, timestep and transmembrane potential by the
+  // model-specific scaling factors.
+  const double t = Ts / Tscale;
+  const double dt = Ti / Tscale;
+  X(0) = (X(0) - Voffset) / Vscale;
+
+  const double fext = (Istim + Isac) * Tscale / Vscale;
+
+  Vector<double> Xrk(nX);
+  Vector<double> frk1(nX), frk2(nX), frk3(nX), frk4(nX);
+
+  // First RK stage.
+  Xrk = X;
+  getf(nX, Xrk, frk1, fext);
+
+  // Second RK stage.
+  Xrk = X + 0.5 * dt * frk1;
+  getf(nX, Xrk, frk2, fext);
+
+  // Third RK stage.
+  Xrk = X + 0.5 * dt * frk2;
+  getf(nX, Xrk, frk3, fext);
+
+  // Fourth RK stage.
+  Xrk = X + dt * frk3;
+  getf(nX, Xrk, frk4, fext);
+
+  X = X + dt / 6.0 * (frk1 + 2.0 * (frk2 + frk3) + frk4);
+
+  // Bring the potential variable back to dimensional units.
+  X(0) = X(0) * Vscale + Voffset;
+}
---
+++ Code/Source/solver/ionic_model.h
@@ -0,0 +1,172 @@
+// SPDX-FileCopyrightText: Copyright (c) Stanford University, The Regents of the
+// University of California, and others. SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef IONIC_MODEL_H
+#define IONIC_MODEL_H
+
+#include "Array.h"
+#include "Vector.h"
+
+/**
+ * @brief Abstract ionic model class.
+ *
+ * ### Mathematical model
+ *
+ * This class represents an abstract ionic model, i.e. an ODE system in the form
+ * @f[ \begin{aligned}
+ *   \frac{\partial v}{\partial t} + I_\text{ion}(v, \mathbf{w}) &=
+ *      I_\text{ext}(t) \\
+ *   \frac{\partial \mathbf{w}}{\partial t} &= \mathbf{F}(v, \mathbf{w})
+ * \end{aligned} @f]
+ * where @f$v@f$ is the transmembrane potential, @f$\mathbf{w}@f$ is a vector of
+ * ionic model variables (which may be gating variables or ionic
+ * concentrations), @f$I_\text{ion}@f$ is the ionic current, and
+ * @f$I_\text{ext}@f$ is an externally applied current.
+ *
+ * Individual models differ in the number of state variables @f$\mathbf{w}@f$
+ * and in the expressions of @f$I_\text{ion}@f$ and @f$\mathbf{F}@f$. These are
+ * specified by classes derived from this.
+ *
+ * **References**:
+ *  - Colli Franzone, Pavarino, Scacchi. Mathematical Cardiac Electrophysiology.
+ *    Springer, 2014.
+ *
+ * @todo Document stretch-activated current.
+ *
+ * ### Numerical methods
+ *
+ * @todo
+ *
+ * ### Implementation details
+ *
+ * @todo
+ *
+ */
+class IonicModel {
+public:
+  /// Constructor.
+  IonicModel(const double Vrest_)
+      : Vrest(Vrest_), Vscale(1.0), Tscale(1.0), Voffset(0.0) {}
+
+  /// Constructor with scaling factors.
+  IonicModel(const double Vrest_, const double Vscale_, const double Tscale_,
+             const double Voffset_)
+      : Vrest(Vrest_), Vscale(Vscale_), Tscale(Tscale_), Voffset(Voffset_) {}
+
+  /// Virtual destructor.
+  virtual ~IonicModel() = default;
+
+  /**
+   * @brief Setup model initial conditions.
+   *
+   * @param[in] nX Number of state variables.
+   * @param[out] X Vector of state variables to be initialized.
+   */
+  virtual void init(const int nX, Vector<double> &X) const = 0;
+
+  /**
+   * @brief Setup model initial conditions.
+   *
+   * @param[in] nX Number of state variables.
+   * @param[out] X Vector of state variables to be initialized.
+   * @param[in] X0 Value to be assigned to all the state variables.
+   */
+  void init(const int nX, Vector<double> &X, double X0) const;
+
+  /**
+   * @brief Setup model initial conditions.
+   *
+   * @param[in] nX Number of state variables.
+   * @param[out] X Vector of state variables to be initialized.
+   * @param[in] X0 Vector of initial state values.
+   */
+  void init(const int nX, Vector<double> &X, const Vector<double> &X0) const;
+
+  /**
+   * @name Integration methods.
+   * @{
+   */
+
+  /**
+   * @brief Integrate the model with the Crank-Nicolson method.
+   *
+   * @todo Document numerical formulation.
+   */
+  void integ_cn2(const int nX, Vector<double> &Xn, const double Ts,
+                 const double Ti, const double Istim, const double Ksac,
+                 Vector<int> &IPAR, Vector<double> &RPAR) const;
+
+  /**
+   * @brief Integrate the model with the forward Euler method.
+   *
+   * @todo Document numerical formulation.
+   */
+  void integ_fe(const int nX, Vector<double> &X, const double Ts,
+                const double Ti, const double Istim, const double Ksac) const;
+
+  /**
+   * @brief Integrate the model with the Runge-Kutta method.
+   *
+   * @todo Document numerical formulation.
+   */
+  void integ_rk(const int nX, Vector<double> &X, const double Ts,
+                const double Ti, const double Istim, const double Ksac) const;
+
+  /**
+   * @}
+   */
+
+protected:
+  /**
+   * @brief Model right hand side.
+   *
+   * Defines the right-hand side function for the potential and ionic equations.
+   * Must be ovverridden in derived classes.
+   *
+   * @todo Document the meaning of the individual parameters.
+   */
+  virtual void getf(const int n, const Vector<double> &X, Vector<double> &f,
+                    const double fext) const = 0;
+
+  /**
+   * @brief Model jacobian.
+   *
+   * Defines the jacobian matrix of the model equations, that is the matirx of
+   * derivatives of the function evaulated by getf.
+   *
+   * @todo Document the meaning of the individual parameters.
+   */
+  virtual void getj(const int n, const Vector<double> &X, Array<double> &Jac,
+                    const double Ksac) const = 0;
+
+  /// Resting transmembrane potential. It is used to define the
+  /// stretch-activated current.
+  const double Vrest;
+
+  /**
+   * @name Scaling factors.
+   *
+   * Individual ionic models may need to rescale the time or voltage variable,
+   * e.g. to bring them into dimensionless form. These are the factors used for
+   * that purpose. They are assigned in the constructor of this class.
+   *
+   * @todo Document units of measure.
+   *
+   * @{
+   */
+
+  /// Voltage scaling.
+  const double Vscale;
+
+  /// Time scaling.
+  const double Tscale;
+
+  /// Voltage offset parameter.
+  const double Voffset;
+
+  /**
+   * @}
+   */
+};
+
+#endif
