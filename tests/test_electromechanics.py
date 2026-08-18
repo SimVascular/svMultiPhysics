3e182037adef39f7f6ff89b912d5e8579193c9f2
Sami Bismar <sbismar2025@gmail.com>
2026-08-18 16:01:12 -0500

    Parametrize electromechanics slab tests

--- tests/test_electromechanics.py
+++ tests/test_electromechanics.py
@@ -1,3 +1,5 @@
+import pytest
+
 from .conftest import run_with_reference
 
 # Common folder for all tests in this file
@@ -21,13 +23,8 @@ fields = [
 ]
 
 
-def test_slab(n_proc):
+@pytest.mark.parametrize("model", ["NashPanfilov", "Regazzoni"])
+def test_slab(model, n_proc):
     run_with_reference(base_folder, "slab", fields, n_proc, t_max=1,
-                       name_inp="solver_NashPanfilov.xml",
-                       name_ref="result_NashPanfilov_001.vtu")
-
-
-def test_slab_regazzoni(n_proc):
-    run_with_reference(base_folder, "slab", fields, n_proc, t_max=1,
-                       name_inp="solver_Regazzoni.xml",
-                       name_ref="result_Regazzoni_001.vtu")
+                       name_inp=f"solver_{model}.xml",
+                       name_ref=f"result_{model}_001.vtu")
